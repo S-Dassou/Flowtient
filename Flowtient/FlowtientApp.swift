ba4525147d2473d418e9dc1af7b7200fc1859881
Shafique Dassu <shafd1231@gmail.com>
2024-03-14 23:15:14 +0000

    Initial Commit

---
+++ Flowtient/FlowtientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowtientApp.swift
+//  Flowtient
+//
+//  Created by shafique dassu on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlowtientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flowtient/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Flowtient
+//
+//  Created by shafique dassu on 14/03/2024.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
